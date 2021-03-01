ef9cb512b4728629f34104f338b7449fb75d137d
Sandip Swain <48983227+sandipswain@users.noreply.github.com>
2021-03-01 07:49:25 +0530

    Update script.js

--- Clock/script.js
+++ Clock/script.js
@@ -1,8 +1,8 @@
 setInterval(setClock, 1000);
 
-const hourHand = document.querySelector("[data-hour-hand]");
-const minuteHand = document.querySelector("[data-minute-hand]");
-const secondHand = document.querySelector("[data-second-hand]");
+const hourHand = document.querySelector(".data-hour-hand");
+const minuteHand = document.querySelector(".data-minute-hand");
+const secondHand = document.querySelector(".data-second-hand");
 
 function setClock() {
   const currentDate = new Date();
